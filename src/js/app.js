b5a0276209017f8abb4af1f1a6a29f1af84e500b
Amadeusz <amikusek123@gmail.com>
2021-03-02 19:44:51 +0100

    poprawa bledow

--- src/js/app.js
+++ src/js/app.js
@@ -56,30 +56,70 @@ toggle.addEventListener("click", () => {
 
 // fetching
 const baseUrl = `https://api.shrtco.de/v2/shorten?url=`;
+
 const form = document.getElementById("form");
 const text = document.getElementById("text");
 const list = document.getElementById("list");
-const copy = (short,but) =>{
+const trick= document.getElementById('trick')
+let clipboard;
+
+const shortUrlCall = (inp) => {
+  const loading = document.getElementById('loading')
+  loading.style="visibility:visible;height:84px"
+  fetch(baseUrl + inp, {
+    method: "GET",
+  })
+    .then((data) => data.json())
+    .then((json) => {
+      if (json.ok) {
+        console.log(json.result);
+
+        addToLocal(json.result.original_link, json.result.short_link);
+      } else {
+              // tu daj czerwony outline
+        console.log(json);
+        trick.classList.add('input-error')
+        text.addEventListener('input',removeError)
+      }
+    })
+    .catch((err) => {
+      console.log(err, "blad serwera");
+    });
+    loading.style=""
+};
+// COPYING
+// changes the style so the users knows that he copied
+const copy = (short, but) => {
   let curr = localStorage.getItem("pages");
   curr = JSON.parse(curr);
-  const notCopy = og =>{
+  const notCopy = (og) => {
     return og.short === short;
-  }
-  curr = curr.filter(notCopy)
-  console.log(curr,but)
-  but.style="background-color:hsl(257, 27%, 26%);"
-  but.innerHTML = "Copied!"
+  };
+  curr = curr.filter(notCopy);
+  // console.log(curr,but)
+  but.style = "background-color:var(--VDViolet);";
+  but.innerHTML = "Copied!";
 
-  setTimeout(()=>{
-    but.style=""
-    but.innerHTML = "Copy it!"
-  },3000)
-
-}
+  setTimeout(() => {
+    but.style = "";
+    but.innerHTML = "Copy it!";
+  }, 3000);
+};
+// USES LIBRARY TO COPY 
+const clipUpdate = () => {
+  const copyButtons = document.getElementsByClassName("copy");
+  clipboard = new ClipboardJS(copyButtons);
+  clipboard.on("success", function (e) {
+  e.clearSelection();
+  });
+  clipboard.on("error", function (e) {
+    console.log(e);
+  });
+};
+// CHANGES IN HTML
 // this function adds element to html, shoudl be reun after every call and upon refreshing
 const addToHTML = (og, short) => {
   // it is used to get item index at the time of creation
-  console.log('createrd')
   let curr = localStorage.getItem("pages");
   curr = JSON.parse(curr);
   // static link
@@ -91,13 +131,18 @@ const addToHTML = (og, short) => {
   // copy link
   const sl = document.createElement("span");
   const cop = document.createElement("button");
-  sl.classList = "second-link";
+
+  sl["data-clipboard-text"] = short;
+  sl.classList = "second-link copy";
   sl.innerHTML = short;
-  sl.addEventListener('click',()=>copy(short,cop))
-  // copy buttonm
+  sl.setAttribute('data-clipboard-text',short)
+  sl.addEventListener("click", () => copy(short, cop));
+// copy button
+  cop.text = short;
   cop.classList = "square copy";
   cop.innerHTML = "Copy&nbsp;it!";
-  cop.addEventListener('click',()=>copy(short,cop))
+  cop.setAttribute('data-clipboard-text',short)
+  cop.addEventListener("click", () => copy(short, cop));
 
   // delete button
   const del = document.createElement("button");
@@ -106,42 +151,42 @@ const addToHTML = (og, short) => {
   del.addEventListener("click", () => {
     removeFromLocal(short);
     li.style = "margin-left:300vw; opacity:0";
-    goUp(short)
-    setTimeout(()=>{
-      li.remove()
-    },1000)
+    goUp(short);
+    setTimeout(() => {
+      li.remove();
+    }, 1000);
   });
   li.appendChild(fl);
   li.appendChild(sl);
   li.appendChild(cop);
   li.appendChild(del);
-  list.appendChild(li);
+  // list.appendChild(li);
+  list.insertBefore(li, list.firstChild);
+  // calls this to update object
+  clipUpdate();
 };
 // animate all from certain short name to go up
-const goUp = short =>{
+const goUp = (short) => {
   const items = Array.from(document.getElementsByClassName("saved-link"));
-  // this is array of all items
   // znajduje indeks elementu o pewnej nazwie, wszystkie nziej animuje
-  const arr = []
-  items.forEach(item=>{
-    console.log(item.children[1])
-    arr.push(item.children[1])
-  })
+  const arr = [];
+  items.forEach((item) => {
+    // console.log(item.children[1])
+    arr.push(item.children[1]);
+  });
   let maxIndex;
-  arr.forEach((item,index)=>{
-    if(item.innerHTML===short){
+  arr.forEach((item, index) => {
+    if (item.innerHTML === short) {
       maxIndex = index;
     }
-  })
-  // console.log(arr,maxIndex)
-  for(let i=maxIndex+1;i<items.length;i++){
-    items[i].style="transform: translateY(-94px); transition:  1s;"
-    setTimeout(()=>{
-      items[i].style="transition: 0s;"
-    },1000)
+  });
+  for (let i = maxIndex + 1; i < items.length; i++) {
+    items[i].style = "transform: translateY(-94px); transition:  1s;";
+    setTimeout(() => {
+      items[i].style = "transition: 0s;";
+    }, 1000);
   }
-  
-}
+};
 //add item to local storage based on results
 const addToLocal = (og, short) => {
   console.log(og, short);
@@ -155,40 +200,25 @@ const addToLocal = (og, short) => {
     entry = JSON.stringify(entry);
     localStorage.setItem("pages", [entry]);
     addToHTML(og, short);
-
   } else {
     let curr = localStorage.getItem("pages");
     curr = JSON.parse(curr);
     curr.push(entry);
     localStorage.setItem("pages", JSON.stringify(curr));
     addToHTML(og, short);
-
   }
 };
-
-const shortUrlCall = (inp) => {
-  fetch(baseUrl + inp, {
-    method: "GET",
-  })
-    .then((data) => data.json())
-    .then((json) => {
-      if (json.ok) {
-        console.log(json.result);
-        addToLocal(json.result.original_link, json.result.short_link);
-      } else {
-        console.log(json);
-      }
-    })
-    .catch((err) => {
-      console.log(err, "blad serwera");
-    });
-};
-
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   shortUrlCall(text.value);
 });
+
+const removeError = ()=>{
+  trick.classList.remove('input-error')
+  console.log('should fire once')
+  text.removeEventListener('input',removeError)
+
+}
 
 if (!localStorage.getItem("pages")) {
   localStorage.setItem("pages", []);
@@ -196,24 +226,20 @@ if (!localStorage.getItem("pages")) {
 
 if (localStorage.getItem("pages").length > 0) {
   const localArray = JSON.parse(localStorage.getItem("pages"));
-  localArray.forEach((item,index) => {
+  localArray.forEach((item, index) => {
     addToHTML(item.long, item.short);
   });
 }
 
-// const items = Array.from(document.getElementsByClassName("saved-link"));
-// const links = Array.from(document.getElementsByClassName("second-link"));
-// const copies = Array.from(document.getElementsByClassName("copy"));
-// const deletes = Array.from(document.getElementsByClassName("delete"));
 const removeFromLocal = (short) => {
   // zamienie na array potem podimeinie i usune
   let curr = localStorage.getItem("pages");
   curr = JSON.parse(curr);
-  console.log(curr)
-  const notCopy = og =>{
+  console.log(curr);
+  const notCopy = (og) => {
     return og.short !== short;
-  }
-  curr = curr.filter(notCopy)
-  console.log(curr)
+  };
+  curr = curr.filter(notCopy);
+  console.log(curr);
   localStorage.setItem("pages", JSON.stringify(curr));
 };
